a4beeb64780f0931d40f1aa9f32b990044fc4344
FlorinAlexCh <chirut.alex@gmail.com>
2022-01-09 19:27:12 +0200

    first commit fibonacci

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int aux = a;
+                a = b;
+                b = aux + b;
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+            Console.Write("Adauga lungimea sirului: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Rezultatul de la {input} este ", input);
+            for (int i = 0; i < input; i++)
+            {
+                Console.Write("{0} ", Fibonacci(i));
+            }
+        }
+    }
+}
