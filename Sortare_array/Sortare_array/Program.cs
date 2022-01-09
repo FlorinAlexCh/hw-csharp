f1f6aa0240282265beaeb648315c5f21248315c5
FlorinAlexCh <chirut.alex@gmail.com>
2022-01-09 19:00:48 +0200

    Sortare Array

---
+++ Sortare_array/Sortare_array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Sortare_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4, 6, 8, 34, 7 };
+            Console.WriteLine("Scrie 1 pentru sortare ascendenta a array-ului sau 2 pentru sortare descendenta");
+            var input = Console.ReadLine();
+            int input1 = Int32.Parse(input);
+            if (input1 == 1)
+            {
+                Array.Sort(array);
+                foreach (int asc in array)
+                {
+                    Console.WriteLine(asc);
+                }
+            }
+            else if (input1 == 2)
+            {
+                array = array.OrderByDescending(c => c).ToArray();
+                foreach (int reverse in array)
+                {
+                    Console.WriteLine(reverse);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number entered wasn't 1 or 2, your loss");
+            }
+        }
+    }
+}
