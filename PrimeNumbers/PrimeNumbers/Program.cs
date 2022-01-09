ff28b661d7cbd9473f99dedd5f487005889bd82e
FlorinAlexCh <chirut.alex@gmail.com>
2022-01-09 19:46:30 +0200

    first commit prime numbers

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static bool isPrime(int n)
+        {
+            if (n == 1 || n == 0) return false;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diplay all prime numbers until:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                if (isPrime(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
