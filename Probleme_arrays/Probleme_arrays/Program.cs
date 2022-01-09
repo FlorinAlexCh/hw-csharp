9ed3735f423fe7327a6cab27c85bcf2884e3c579
FlorinAlexCh <chirut.alex@gmail.com>
2022-01-09 18:34:45 +0200

    Probleme arrays

--- Probleme_arrays/Probleme_arrays/Program.cs
+++ Probleme_arrays/Probleme_arrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Probleme_arrays
 {
@@ -6,7 +7,28 @@ namespace Probleme_arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            // Elementul maxim intr-un array 
+            int maxNumber = array.Max();
+            Console.WriteLine($"Max number of this array is: {maxNumber}");
+
+            // Elementul minim intr-un array
+            int minNumber = array.Min();
+            Console.WriteLine($"Max number of this array is: {minNumber}");
+
+            // Display the Index from a specific number
+            Console.WriteLine("Please write a number from 1 to 7");
+            var item = Console.ReadLine();
+            int item1 = Int32.Parse(item);
+            var index1 = Array.IndexOf(array, item1);
+            Console.WriteLine("Your number can be found at index " + index1);
+
+            // Media aritmetica
+
+            double average = array.Average();
+            Console.WriteLine($"The average is {average}");
         }
+
     }
 }
