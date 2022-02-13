46a4dc92e8adbb1707d65bd3ff3ec2b1d99aa220
FlorinAlexCh <chirut.alex@gmail.com>
2022-02-13 19:12:49 +0200

    Sum of an array (recursiv)

---
+++ Vector-Minim/RecursiveSum/RecursiveSum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RecursiveSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 3, 6, 8, 9, 5};
+            int n = array.Length;
+            int result = SumOfArray.CalculateSum(array, n);
+
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
---
+++ Vector-Minim/RecursiveSum/RecursiveSum/SumOfArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveSum
+{
+     public static class SumOfArray
+    {
+        public static int CalculateSum(int[] array, int n)
+        {
+            if (n <= 0 || n > array.Length)
+                return 0;
+            else
+                return CalculateSum(array, n - 1) + array[n - 1];
+            
+
+        }
+    }
+}
